80326f8d657e3b882fcb9b3ede43aa6976308886
dongdongey <bananakenny@icloud.com>
2024-10-09 19:40:31 +0900

    add member(None) at enum KindOfBatchim

--- src/has_batchim/has_batchim.rs
+++ src/has_batchim/has_batchim.rs
@@ -3,9 +3,10 @@ use crate::_internal::constants::{self, get_disassembled_jongseong};
 pub enum KindOfBatchim {
     Single,
     Double,
+    None,
 }
 
-pub fn has_batchim(guljas: &str, option: Option<KindOfBatchim>) -> bool {
+pub fn has_batchim(guljas: &str, option: KindOfBatchim) -> bool {
     let last_char = match guljas.chars().last() {
         Some(a) => a,
         None => {
@@ -26,27 +27,23 @@ pub fn has_batchim(guljas: &str, option: Option<KindOfBatchim>) -> bool {
         (char_code - constants::COMPLETE_HANGUL_START_CHARCODE) % constants::NUMBER_OF_JONGSEONG;
 
     match option {
-        Some(kind_of_batchim) => {
-            match kind_of_batchim {
-                KindOfBatchim::Single => {
-                    if let Some(disassembled) =
-                        get_disassembled_jongseong(constants::JONGSEONGS[jongseong_index])
-                    {
-                        return disassembled.chars().count() == 1;
-                    }
-                    return false;
-                }
-                KindOfBatchim::Double => {
-                    if let Some(disassembled) =
-                        get_disassembled_jongseong(constants::JONGSEONGS[jongseong_index])
-                    {
-                        return disassembled.chars().count() == 2;
-                    }
-                    return false;
-                }
-            };
+        KindOfBatchim::Single => {
+            if let Some(disassembled) =
+                get_disassembled_jongseong(constants::JONGSEONGS[jongseong_index])
+            {
+                return disassembled.chars().count() == 1;
+            }
+            return false;
         }
-        None => {
+        KindOfBatchim::Double => {
+            if let Some(disassembled) =
+                get_disassembled_jongseong(constants::JONGSEONGS[jongseong_index])
+            {
+                return disassembled.chars().count() == 2;
+            }
+            return false;
+        }
+        KindOfBatchim::None => {
             return jongseong_index > 0;
         }
     };
@@ -58,9 +55,9 @@ mod tests {
     use crate::has_batchim::has_batchim::KindOfBatchim;
     #[test]
     fn batchim___() {
-        assert_eq!(has_batchim("앗", None), true);
-        assert_eq!(has_batchim("흙", Some(KindOfBatchim::Double)), true);
-        assert_eq!(has_batchim("흙", Some(KindOfBatchim::Single)), false);
-        assert_eq!(has_batchim("어", None), false);
+        assert_eq!(has_batchim("앗", KindOfBatchim::None), true);
+        assert_eq!(has_batchim("흙", KindOfBatchim::Double), true);
+        assert_eq!(has_batchim("흙", KindOfBatchim::Single), false);
+        assert_eq!(has_batchim("어", KindOfBatchim::None), false);
     }
 }
